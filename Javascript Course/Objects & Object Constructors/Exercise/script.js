38545512568a57766391948d62c2c36b1c392777
Sentriarc <tran.christopherd1@gmail.com>
2024-09-09 19:13:42 -0500

    added basic function

--- Javascript Course/Objects & Object Constructors/Exercise/script.js
+++ Javascript Course/Objects & Object Constructors/Exercise/script.js
@@ -1,9 +1,19 @@
 function Book(title, author, pages, alreadyRead) {
-    // this.title = title;
-    // this.author = author;
-    // this.pages = pages;
-    // let alreadyRead = false;
-    // this.alreadyRead = alreadyRead;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    let alreadyRead = false;
+    this.alreadyRead = alreadyRead;
+
+    let info = function(book) {
+        if (/*variable*/) {
+            return Book;
+        }
+        else {
+            return ("Invalid book!");
+        }
+    };
+    
     Book.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
     return theHobbit.info();
 }
