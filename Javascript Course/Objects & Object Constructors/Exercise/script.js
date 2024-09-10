1f690181acc87d70aafe638a84e0490d974aec10
Sentriarc <tran.christopherd1@gmail.com>
2024-09-09 19:09:18 -0500

    added JS

---
+++ Javascript Course/Objects & Object Constructors/Exercise/script.js
@@ -0,0 +1,9 @@
+function Book(title, author, pages, alreadyRead) {
+    // this.title = title;
+    // this.author = author;
+    // this.pages = pages;
+    // let alreadyRead = false;
+    // this.alreadyRead = alreadyRead;
+    theHobbit.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+    return theHobbit.info();
+}
